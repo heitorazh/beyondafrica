9b40774fdac88d26a84ff4465c93591f4c7f6550
Heitor Oliveira Lima <125208262+heitorazh@users.noreply.github.com>
2026-06-30 21:44:38 -0300

    Add files via upload

---
+++ sub/script.js
@@ -0,0 +1,18 @@
+console.log("Script carregado!");
+
+let lastScroll = 0;
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    const currentScroll = window.pageYOffset;
+
+    if(currentScroll > lastScroll && currentScroll > 100){
+        header.classList.add("hide");
+    }else{
+        header.classList.remove("hide");
+    }
+
+    lastScroll = currentScroll;
+
+});
